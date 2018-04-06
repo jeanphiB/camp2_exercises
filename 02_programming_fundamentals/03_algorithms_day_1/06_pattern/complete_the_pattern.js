9067111983b7e8f58d67e1caa4e9b91c8c38447b
Jean-Philippe Bornier <jeanphilippe.bornier@decathlon.com>
2018-04-06 15:06:35 +0200

    Exercice 06_pattern is complete

--- 02_programming_fundamentals/03_algorithms_day_1/06_pattern/complete_the_pattern.js
+++ 02_programming_fundamentals/03_algorithms_day_1/06_pattern/complete_the_pattern.js
@@ -1,3 +1,12 @@
 // Modify this value to test with other values
 const numberOfLine = 5;
 // Your code here ⬇
+let rowArray = [];
+for (let row = 1; row <= numberOfLine; row++) {
+  let rowString = "";
+  for (let col = numberOfLine; col >= row; col--) {
+    rowString += col;
+  }
+  rowArray.push(rowString);
+}
+console.log(rowArray.join("\n"));
