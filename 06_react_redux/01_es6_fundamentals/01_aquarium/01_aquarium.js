144acf274211567c10e835a0917326e379819c2a
Jean-Philippe Bornier <jeanphilippe.bornier@decathlon.com>
2018-05-22 14:16:56 +0200

    6_react_redux/01_es6_fundamentals completed

--- 06_react_redux/01_es6_fundamentals/01_aquarium/01_aquarium.js
+++ 06_react_redux/01_es6_fundamentals/01_aquarium/01_aquarium.js
@@ -1,6 +1,69 @@
 // Insert code here
+class Aquarium {
+  constructor(fishes, algaes) {
+    this.fishes = fishes;
+    this.algaes = algaes;
+  }
 
+  addFish(fish) {
+    this.fishes.push(fish);
+  }
 
+  addAlgae(algae) {
+    this.algaes.push(algae);
+  }
+
+  passTime() {
+    console.log("Algaes number: ", this.algaes.length);
+    this.fishes.forEach(fish => console.log("Fish name: ", fish.name, " - sex: ", fish.sex));
+    this.fishes.forEach((fish, thisArg) => {
+      fish.eat(this, thisArg);
+    });
+  }
+
+  endTurn() {
+    while(this.fishes.length > 1) {
+      this.passTime();
+    }
+  }
+}
+
+class Fish {
+  constructor(name, sex, vegan) {
+    this.name = name;
+    this.sex = sex;
+    this.vegan = vegan;
+  }
+
+  eat(aquarium, thisArg) {
+    if (this.vegan) {
+      aquarium.algaes.pop();
+      console.log("1 algea eated. Let ", aquarium.algaes.length);
+    } else {
+      let eaten = false;
+      while(aquarium.fishes.length > 1 && !eaten) {
+        const randomFish = Math.round(Math.random() * aquarium.fishes.length);
+        if (aquarium.fishes[randomFish] !== thisArg) {
+          aquarium.fishes.splice(randomFish, 1);
+          console.log("fish id", randomFish, " eaten. Let ", aquarium.fishes.length);
+          eaten = true;
+        }
+      }
+    }
+  }
+}
+
+class Algae {
+  constructor(size) {
+    this.size = size;
+  }
+}
+
+// const newCarnFish = new Fish("Lorem", "M", 0);
+// const newVeganFish = new Fish("Lorem", "M", 1);
+// const newAlgae = new Algae(12);
+// const newAquarium = new Aquarium([newVeganFish, newCarnFish], [newAlgae]);
+// newAquarium.endTurn();
 
 module.exports = {
   aquarium: Aquarium,
--- 06_react_redux/01_es6_fundamentals/02_spread_destructuring/02_spread_destructuring.js
+++ 06_react_redux/01_es6_fundamentals/02_spread_destructuring/02_spread_destructuring.js
@@ -1,7 +1,8 @@
+/*
 function go(options) {
   let {
     speed = 4,
-    enable: { hyperdrive = false, frobnifier = true}
+    enable: { hyperdrive = false, frobnifier = true} = {}
   } = options;
 
   console.log("speed=", speed, "hyperdrive:", hyperdrive, "frobnifier:", frobnifier);
@@ -9,7 +10,7 @@ function go(options) {
 
 go({ speed: 5 });
 
-function lastIndexOf(arr, elt, start) {
+function lastIndexOf(arr, elt, start = arr.length) {
   for (let i = start - 1; i >= 0; i--) {
     if (arr[i] === elt) {
       return i;
@@ -20,17 +21,20 @@ function lastIndexOf(arr, elt, start) {
 }
 
 console.log(lastIndexOf([1, 2, 1, 2], 2));
-
+*/
 function replace(array, from, to, elements) {
-  array.splice.apply(array, [from, to - from].concat(elements));
+  //array.splice.apply(array, [from, to - from].concat(elements));
+  array.splice(...[from, to - from, ...elements]);
 }
 
-let testArray = [1, 2, 100, 100, 6];
+const testArray = [1, 2, 100, 100, 6];
 replace(testArray, 2, 4, [3, 4, 5]);
 console.log(testArray);
-
+/*
 function copyReplace(array, from, to, elements) {
-  return array.slice(0, from).concat(elements).concat(array.slice(to));
+  //return array.slice(0, from).concat(elements).concat(array.slice(to));
+  return [...array.slice(0, from), ...elements, ...array.slice(to)];
 }
 
 console.log(copyReplace([1, 2, 100, 200, 6], 2, 4, [3, 4, 5]));
+*/
